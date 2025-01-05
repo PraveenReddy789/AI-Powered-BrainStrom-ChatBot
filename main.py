c8d32c0ce4622f857d23b5d395a169bea9bf4c2a
PraveenReddy789 <214g5a3203@srit.ac.in>
2025-01-05 12:11:44 +0530

    Python File

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ st.markdown("Generate ideas, solve problems, and explore creative solutions with
 
 # Hugging Face token input
 if "HF_TOKEN" not in st.session_state:
-    st.session_state["HF_TOKEN"] = "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"
+    st.session_state["HF_TOKEN"] = ""
 
 if "history" not in st.session_state:
     st.session_state["history"] = []  # To store question-answer pairs
